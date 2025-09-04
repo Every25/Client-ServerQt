df2a8eb68292455b7e8709da3d976d7f0497e6b7
Masha <163397765+chiquibamboni@users.noreply.github.com>
2025-09-04 17:50:12 +0700

    Update main.cpp

--- ClientQt/main.cpp
+++ ClientQt/main.cpp
@@ -1,8 +1,10 @@
 ﻿#include "LibrariesWidget.h"
 #include <QtWidgets/QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
+    QApplication::setStyle(QStyleFactory::create("Windows"));
     QApplication app(argc, argv);
     LibrariesWidget window;
     window.setWindowTitle(QStringLiteral(u"Библиотеки элементов"));
